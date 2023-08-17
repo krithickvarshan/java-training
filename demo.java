aa6f4df4c85047c037c80c2866e4af1f31a1667d
krithickvarshan <88022457+krithickvarshan@users.noreply.github.com>
2023-08-17 07:58:32 +0530

    Method

--- demo.java
+++ demo.java
@@ -1,8 +1,26 @@
-class computer{
+class computer {
+
+    public void  playMusic(){
+    System.out.println("Music playing..");
+   }
+   public String getMePen(int money){
+    if(money>=10)
+       return "pen";
+  
+        return "money is lesser than pen value";
+    }
+    
+   
 
 }
+
 public class demo {
-    public static void main(String ags[]){
-        
+
+    public static void main(String ags[]) {
+
+     computer play = new computer();
+     play.playMusic();
+     String result =play.getMePen(3);
+     System.out.println(result);
     }
 }
